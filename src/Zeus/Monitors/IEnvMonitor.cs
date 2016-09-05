4abfd74c452547d6d21b8760a335bd478abf3d1a
tokmac <lyubomir.mitkov@gmail.com>
2016-09-05 17:02:49 +0300

    Added Server Time Info (Current Time, Time Zone and UTC Offset)

--- src/Zeus/Monitors/IEnvMonitor.cs
+++ src/Zeus/Monitors/IEnvMonitor.cs
@@ -7,11 +7,32 @@
 
     public class EnvInfo
     {
-        public EnvInfo(string osName)
+        public EnvInfo(string osName, EnvTimeInfo time)
         {
             OSName = osName;
+            Time = time;
         }
 
         public string OSName { get; private set; }
+
+        public EnvTimeInfo Time { get; private set; }
+
+    }
+
+
+    public class EnvTimeInfo
+    {
+        public EnvTimeInfo(string time, string zone, string utcOffset)
+        {
+            Time = time;
+            TimeZone = zone;
+            UtcOffset = utcOffset;
+        }
+
+        public string Time { get; private set; }
+
+        public string TimeZone { get; private set; }
+
+        public string UtcOffset { get; private set; }
     }
 }
