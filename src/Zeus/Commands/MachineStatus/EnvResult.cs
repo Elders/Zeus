7cd870f08a77e95df9508412b67d9257001c78bf
tokmac <lyubomir.mitkov@gmail.com>
2016-09-07 11:25:10 +0300

    Changed Environment Result Format

--- src/Zeus/Commands/MachineStatus/EnvResult.cs
+++ src/Zeus/Commands/MachineStatus/EnvResult.cs
@@ -7,17 +7,11 @@ namespace Zeus.Commands.MachineStatus
         public EnvResult(EnvInfo env)
         {
             OS = env.OSName;
-            Time = env.Time.Time;
-            TimeZone = env.Time.TimeZone;
-            UtcOffset = env.Time.UtcOffset;
+            TimeInfo = env.Time;
         }
 
         public string OS { get; set; }
 
-        public string Time { get; set; }
-
-        public string TimeZone { get; set; }
-
-        public string UtcOffset { get; set; }
+        public EnvTimeInfo TimeInfo { get; set; }
     }
 }
